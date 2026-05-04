2b82813754992829fa9cb550c53c283d6e5a4d51
bugg86 <jackfink68@yahoo.com>
2026-05-03 21:41:12 -0400

    update get_user endpoint

--- index.ts
+++ index.ts
@@ -24,12 +24,13 @@ Bun.serve({
     "/get_user": {
       GET: async (req) => {
         const k = new URL(req.url).searchParams.get("k");
+        const u = new URL(req.url).searchParams.get("u");
 
         if (!k) {
           return Response.json({ error: "Missing required query parameter: k" }, { status: 400 });
         }
 
-        const res = await fetch(`${baseUrl}/get_user?k=${k}&type=id&u=2`);
+        const res = await fetch(`${baseUrl}/get_user?k=${k}&type=id&u=${u}`);
         return Response.json(await res.json(), { status: res.status });
       }
     },
